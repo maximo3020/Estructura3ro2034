fa5482c3e0dfeb981b4134cba67fa08a52382e4a
maximo3020 <jmaximo684@gmail.com>
2024-08-28 11:22:59 -0300

    Add files via upload

--- frutas.py
+++ frutas.py
@@ -1,8 +1,14 @@
 frutas={"manzana":{"precio":1500},"banana":{"precio":1000}}
-fruta=input("ingrese la fruta que desea vender:")
-cantidad=float(input("ingrese la cantidad de vender:"))
-if frutas in frutas:
- total=frutas[frutas("precio")] =cantidad
- print(total)
-else:
- print("fruta no existe en la lista")
+total=0
+totaldecompra=0
+while True:
+  fruta=input("ingrese la fruta que desea vender:")
+  cantidad=float(input("ingrese la cantidad a vender:"))
+
+  if fruta in frutas:
+    total=frutas[fruta]["precio"] =cantidad
+    totaldecompra=totaldecompra+total
+    print(f"el total a pagar por{cantidad}kilos de {frutas} es: {total}")
+    print("el precio de",cantidad,"kilos","de",frutas,"es de:",total)
+  else:
+     print("la fruta ingresada no existe en la lista")
