7e8f1f7f9bc62af0a4bbfb7edc842c6312e49d36
maximo3020 <jmaximo684@gmail.com>
2024-11-13 10:39:59 -0300

    Add files via upload

---
+++ diccionariodatoscompletos.py
@@ -0,0 +1,5 @@
+nombrecompleta={"nombre":("maximo","ezequiel","juarez");"edad":14,
+      "direccion":"el algarrobo 2637","actividadfavoritas":["futbol","voley"]}
+for c,v in nombrecompleta.items():
+    print(c, ":",v)
+    print("________________________________________")  
